f97ad15d711858ab1bf3a4c491db0ac2ee6a94b6
Hashan Eranda <hashaneranda@gmail.com>
2021-06-10 09:52:11 +0530

    frontend styleguide input compnent added

--- frontend/services/styleguide/src/hotel-styleguide.js
+++ frontend/services/styleguide/src/hotel-styleguide.js
@@ -1,3 +1,4 @@
 export { default as Button } from "./components/Button/button";
+export { default as Input } from "./components/Input/input";
 export { default as ThemeSwitcher } from "./theme/themeSwitcher";
 export { default as Theme } from "./theme/theme";
