a87fa091a9f42663258baef6ec4bc1d242450d8c
Hashan Eranda <hashaneranda@gmail.com>
2021-06-11 02:19:09 +0530

    code cleanup

--- hotel-backend/gateway/server.js
+++ hotel-backend/gateway/server.js
@@ -2,28 +2,6 @@ const { ApolloServer } = require("apollo-server");
 const { ApolloGateway, RemoteGraphQLDataSource } = require("@apollo/gateway");
 const { getUser } = require("./middleware/auth");
 
-const users = [
-  {
-    id: 1,
-    username: "imhassane",
-    email: "imhassane@test.com",
-    password: "imhassane",
-  },
-  {
-    id: 2,
-    username: "imjannet",
-    email: "imjannet@test.com",
-    password: "imjannet",
-  },
-  {
-    id: 3,
-    username: "christophe",
-    email: "christophe@test.com",
-    password: "christophe",
-  },
-  { id: 4, username: "malik", email: "malik@test.com", password: "malik" },
-];
-
 const gateway = new ApolloGateway({
   serviceList: [
     { name: "users", url: "http://localhost:4001/graphql" },
